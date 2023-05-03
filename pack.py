ebdce80f9662cf5e90a3666263a8a12f9aaad935
aidgamesa <113248631+aidgamesa@users.noreply.github.com>
2023-05-03 19:51:53 +0000

    Update pack.py

--- pack.py
+++ pack.py
@@ -8,20 +8,20 @@ from functions import zipdir, ziptob64, genunpackfile, gencompressedunpackfile
 if len(sys.argv)<2:
     print("usage: {} <directory-for-pack>".format(sys.argv[0]))
     sys.exit(1)
-dir_to_pack=sys.argv[1]
-if not dir_to_pack.endswith("/"):
-    dir_to_pack+="/"
+dir_pack_str=sys.argv[1]
+if not dir_pack_str.endswith("/"):
+    dir_pack_str+="/"
 
-dtp=Path(dir_to_pack)
-if not dtp.exists():
-    print("directory {} don't exists".format(dir_to_pack))
+dir_pack=Path(dir_pack_str)
+if not dir_pack.exists():
+    print("directory {} don't exists".format(dir_pack_str))
 
-p=Path("dist")
-if not p.exists():
+dist=Path("dist")
+if not dist.exists():
     os.mkdir("dist")
 
-dtb_name=os.path.dirname(dir_to_pack)
-zipdir(dir_to_pack, f"dist/{dtb_name}.zip")
-ziptob64(f"dist/{dtb_name}.zip", f"dist/{dtb_name}.zip.base64")
-genunpackfile(f"dist/{dtb_name}.py", f"dist/{dtb_name}.zip.base64")
-gencompressedunpackfile(f"dist/{dtb_name}.compressed.py", f"dist/{dtb_name}.py")
+dir_pack_name=os.path.dirname(dir_pack)
+zipdir(dir_to_pack, f"dist/{dir_pack_name}.zip")
+ziptob64(f"dist/{dir_pack_name}.zip", f"dist/{dir_pack_name}.zip.base64")
+genunpackfile(f"dist/{dir_pack_name}.py", f"dist/{dir_pack_name}.zip.base64")
+gencompressedunpackfile(f"dist/{dir_pack_name}.compressed.py", f"dist/{dir_pack_name}.py")
