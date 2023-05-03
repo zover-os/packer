4c7ced14be4f5d012b8c5f5f3ccefb96d96b9fa5
aidgamesa <113248631+aidgamesa@users.noreply.github.com>
2023-05-03 19:54:49 +0000

    Update pack.py

--- pack.py
+++ pack.py
@@ -8,20 +8,20 @@ from functions import zipdir, ziptob64, genunpackfile, gencompressedunpackfile
 if len(sys.argv)<2:
     print(f"usage: {sys.argv[0]} <directory-for-pack>")
     sys.exit(1)
-dirPackStr=sys.argv[1]
-if not dirPackStr.endswith("/"):
-    dirPackStr+="/"
 
-dirPack=Path(dirPackStr)
+if not sys.argv[1]:
+    sys.argv[1]+="/"
+
+dirPack=Path(sys.argv[1])
 if not dirPack.exists():
-    print("directory {} don't exists".format(dirPackStr))
+    print(f"directory {sys.argv[1]} don't exists")
 
 dist=Path("dist")
 if not dist.exists():
     os.mkdir("dist")
 
 dirPackName=os.path.dirname(dirPack)
-zipdir(dirPackStr, f"dist/{dirPackName}.zip")
+zipdir(sys.argv[1], f"dist/{dirPackName}.zip")
 ziptob64(f"dist/{dirPackName}.zip", f"dist/{dirPackName}.zip.base64")
 genunpackfile(f"dist/{dirPackName}.py", f"dist/{dirPackName}.zip.base64")
 gencompressedunpackfile(f"dist/{dirPackName}.compressed.py", f"dist/{dirPackName}.py")
